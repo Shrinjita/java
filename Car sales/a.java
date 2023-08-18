bea8af80f2be117d123b1e418c91491f30bf78f7
Shrinjita Paul <113103022+Shrinjita@users.noreply.github.com>
2023-08-19 04:18:48 +0530

    Add files via upload

---
+++ Car sales/a.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class a here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class a
+{int c;
+    public a()
+    {
+        // initialise instance vx = 0;
+    }
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
